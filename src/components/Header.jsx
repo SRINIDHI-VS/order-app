034b98ff7da0284d7ca792c389574983367225a2
zenz-srinidhi <srinidhi@zenz.tech>
2025-11-24 15:16:24 +0530

    fix: fixed the margin issue on header and main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 export default function Header() {
   return (
-    <header className="w-full max-w-full sm:max-w-7xl mx-auto bg-blue-800 shadow-lg border-b border-gray-200 rounded-2xl mt-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <div>
-            <h1 className="text-2xl font-extrabold text-white tracking-tight">
-              OrderHub
-            </h1>
-            <p className="text-sm text-gray-300 -mt-0.5">
-              Smart Order Management Dashboard
-            </p>
+    <div className="mt-2 mx-2">
+      <header className="w-full max-w-full sm:max-w-7xl mx-auto bg-blue-800 shadow-lg border-b border-gray-200 rounded-2xl mt-4">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <div>
+              <h1 className="text-2xl font-extrabold text-white tracking-tight">
+                OrderHub
+              </h1>
+              <p className="text-sm text-gray-300 -mt-0.5">
+                Smart Order Management Dashboard
+              </p>
+            </div>
           </div>
-
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
